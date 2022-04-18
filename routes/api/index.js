30a9196580f23e9658f1e172c48301ce5bfaf7b8
hishikawa10 <h.ishikawa@g.dreamcareer.co.jp>
2022-04-18 19:03:23 +0900

    API作成

--- routes/api/index.js
+++ routes/api/index.js
@@ -34,16 +34,4 @@ router.patch("/tasks/:id", async function (req, res, next) {
   res.send(patchTasksId);
 });
 
-/* 商品一覧を取得するルーティング */
-router.get("/items", function (req, res, next) {
-  const itemsList = items.getListItem();
-  res.send(itemsList);
-});
-
-/*１件の商品情報を取得するルーティング */
-router.get("/items/:id", function (req, res, next) {
-  const item = items.getItem(req.params.id);
-  res.send(item);
-});
-
 module.exports = router;
